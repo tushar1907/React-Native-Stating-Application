593c92b89767ca91634a1637b6e84f4b647c566a
Tushar <gupta.tus@husky.neu.edu>
2018-10-25 20:53:10 -0400

    Weather app updated

--- BuildApp/src/App.js
+++ BuildApp/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, Animated } from 'react-native';
 
-import { DangerZone } from 'expo';
-const { Lottie } = DangerZone;
+// import { DangerZone } from 'expo';
+// const { Lottie } = DangerZone;
 
 import { API_KEY } from './utils/WeatherAPIKey';
 
